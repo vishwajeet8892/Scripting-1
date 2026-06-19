d5c34861fd00e6f4fdaaa99374c72b71f5c04f91
unknown <vishwajit0892@gmail.com>
2026-06-19 11:33:44 +0530

    INC124578

--- Script1.sh
+++ Script1.sh
@@ -3,5 +3,6 @@ echo "We are from batch-33 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Git"
 echo "I have modified my repo"
-echo "Modified INC123456"
+echo "Modified INC124578"
+echo "This is related to Snow changes"
 
